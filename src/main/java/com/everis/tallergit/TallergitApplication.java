d9538c30564f9b1ff45351f531ef9a8be1b3f0da
Fernando Rodríguez-Izquierdo Serrano <fernando.rodriguez.izquierdo.serrano@everis.com>
2020-05-20 20:53:45 +0200

    Commit inicial del tarller GIT

---
+++ src/main/java/com/everis/tallergit/TallergitApplication.java
@@ -0,0 +1,20 @@
+package com.everis.tallergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallergitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallergitApplication.class, args);
+		start();
+	}
+	
+	public static void start() {
+		
+		System.out.println("Hello world!");
+		
+	}
+
+}
---
+++ src/test/java/com/everis/tallergit/TallergitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.tallergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallergitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
